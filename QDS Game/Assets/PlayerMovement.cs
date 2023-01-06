d480fa5c7598371127508368277f2dcb79ddfd81
rish0303 <107429877+rish0303@users.noreply.github.com>
2023-01-06 00:59:22 -0800

    Added Jumping

--- QDS Game/Assets/PlayerMovement.cs
+++ QDS Game/Assets/PlayerMovement.cs
@@ -1,21 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
     private float horizontal;
     private float speed = 8.0f;
-    private float jumpingPower = 16f;
+    private float jumpingPower = 8f;
+    private float playerHeight = 2.0f;
+
+    bool isGrounded;
 
     [SerializeField] private Rigidbody rb;
 
     // Update is called once per frame
     void Update()
     {
+        isGrounded = Physics.Raycast(transform.position, Vector3.down, playerHeight / 2 + 0.1f);
+
+        print(isGrounded);
+
         horizontal = Input.GetAxisRaw("Horizontal");
 
-        if (Input.GetButtonDown("Jump"))
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
         {
             rb.velocity = new Vector3(rb.velocity.x, jumpingPower, 0.0f);
         }
